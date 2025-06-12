2487b6f46e2b53abf7f63c379422741dcd7fe9c9
Michael Bestas <mkbestas@lineageos.org>
2025-06-12 01:16:00 -0400

    caprip: Override properties for caprip variants
    Co-authored-by: Deivid21 <david.parra.ignacio@gmail.com>
    Change-Id: I8b6ba9a86c887d1743d30b2dfc54343ef7e8d584

--- device.mk
+++ device.mk
@@ -65,6 +65,11 @@ PRODUCT_COPY_FILES += \
 $(foreach DEVICE_SKU, $(DEVICE_COMPASS_SKUS), \
     $(LOCAL_PATH)/permissions/unavail.android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_$(DEVICE_SKU)/unavail.android.hardware.sensor.compass.xml)
 
+# Properties
+PRODUCT_PACKAGES += \
+    carrier.retmea.prop \
+    hardware.sku.XT2129-3.prop
+
 # Shipping API level
 BOARD_SHIPPING_API_LEVEL := 30
 PRODUCT_SHIPPING_API_LEVEL := 30
